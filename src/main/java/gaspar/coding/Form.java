cb6f94a375e2b4a8288af76306db1734bdc3f203
fluor <pawel.d.dabrowski@gmail.com>
2020-08-30 16:43:32 +0200

    Form class established

---
+++ src/main/java/gaspar/coding/Form.java
@@ -0,0 +1,72 @@
+package gaspar.coding;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class Form {
+    Rectangle a;
+    Rectangle b;
+    Rectangle c;
+    Rectangle d;
+    Color color;
+    private String name;
+    public int form = 1;
+
+    public Form(Rectangle a, Rectangle b, Rectangle c, Rectangle d) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+    }
+    public Form(Rectangle a, Rectangle b, Rectangle c, Rectangle d, String name) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+        this.name = name;
+
+        // Set color of the stones
+        switch (name) {
+            case "j" -> {
+                color = Color.SLATEGRAY;
+            }
+            case "l" -> {
+                color = Color.DARKGOLDENROD;
+            }
+            case "o" -> {
+                color = Color.INDIANRED;
+            }
+            case "s" -> {
+                color = Color.FORESTGREEN;
+            }
+            case "t" -> {
+                color = Color.CADETBLUE;
+            }
+            case "z" -> {
+                color = Color.HOTPINK;
+            }
+            case "i" -> {
+                color = Color.SANDYBROWN;
+            }
+        }
+
+        this.a.setFill(color);
+        this.b.setFill(color);
+        this.c.setFill(color);
+        this.d.setFill(color);
+    }
+
+    // getter
+    public String getName() {
+        return name;
+    }
+
+    public void changeForm() {
+        if (form != 4) {
+            form++;
+        } else {
+            form = 1;
+        }
+    }
+
+}
