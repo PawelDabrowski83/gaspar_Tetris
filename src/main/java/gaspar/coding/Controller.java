d5f597af0293824af9bb47cb933d7d3de510e9ec
fluor <pawel.d.dabrowski@gmail.com>
2020-08-31 15:07:47 +0200

    Controller multiple ifs describing different block shapes

--- src/main/java/gaspar/coding/Controller.java
+++ src/main/java/gaspar/coding/Controller.java
@@ -1,5 +1,7 @@
 package gaspar.coding;
 
+import javafx.scene.shape.Rectangle;
+
 public class Controller {
     // getting numbers and MESH from Tetris class
     public static final int MOVE = Tetris.MOVE;
@@ -42,5 +44,74 @@ public class Controller {
         }
     }
 
+    // create the stones
+    public static Form makeRect() {
+        // random color for the stones
+        int block = (int) (Math.random() * 100);
+        String name;
+
+        Rectangle a = new Rectangle(SIZE - 1, SIZE - 1);
+        Rectangle b = new Rectangle(SIZE - 1, SIZE - 1);
+        Rectangle c = new Rectangle(SIZE - 1, SIZE - 1);
+        Rectangle d = new Rectangle(SIZE - 1, SIZE - 1);
+
+        if (block < 15) {
+            a.setX(XMAX / 2 - SIZE);
+            b.setX(XMAX / 2 - SIZE);
+            b.setY(SIZE);
+            c.setX(XMAX / 2);
+            c.setY(SIZE);
+            d.setX(XMAX / 2 + SIZE);
+            d.setY(SIZE);
+            name = "j";
+        } else if (block < 30){
+            a.setX(XMAX / 2 + SIZE);
+            b.setX(XMAX / 2 - SIZE);
+            b.setY(SIZE);
+            c.setX(XMAX / 2);
+            c.setY(SIZE);
+            d.setX(XMAX / 2 + SIZE);
+            d.setY(SIZE);
+            name = "l";
+        } else if (block < 45) {
+            a.setX(XMAX / 2 - SIZE);
+            b.setX(XMAX / 2);
+            c.setX(XMAX / 2 - SIZE);
+            c.setY(SIZE);
+            d.setX(XMAX / 2);
+            d.setY(SIZE);
+            name = "o";
+        } else if (block < 60) {
+            a.setX(XMAX / 2 + SIZE);
+            b.setX(XMAX / 2);
+            c.setX(XMAX / 2);
+            c.setY(SIZE);
+            d.setX(XMAX / 2 - SIZE);
+            d.setY(SIZE);
+            name = "s";
+        } else if (block < 75) {
+            a.setX(XMAX / 2 - SIZE);
+            b.setX(XMAX / 2);
+            c.setX(XMAX / 2);
+            c.setY(SIZE);
+            d.setX(XMAX / 2 + SIZE);
+            name = "t";
+        } else if (block < 90) {
+            a.setX(XMAX / 2 + SIZE);
+            b.setX(XMAX / 2);
+            c.setX(XMAX / 2 + SIZE);
+            c.setY(SIZE);
+            d.setY(XMAX / 2 + SIZE + SIZE);
+            d.setY(SIZE);
+            name = "z";
+        } else {
+            a.setX(XMAX / 2 - SIZE - SIZE);
+            b.setX(XMAX / 2 - SIZE);
+            c.setX(XMAX / 2);
+            d.setX(XMAX / 2 + SIZE);
+            name = "l";
+        }
+        return new Form(a, b, c, d, name);
+    }
 
 }
