665fc9cbd88428a90a1e2c75e907e45620999d60
fluor <pawel.d.dabrowski@gmail.com>
2020-09-03 12:15:00 +0200

    refactor - removed unused code

--- src/main/java/gaspar/coding/Form.java
+++ src/main/java/gaspar/coding/Form.java
@@ -7,7 +7,6 @@ public class Form {
     Square b;
     Square c;
     Square d;
-    Color color;
     private BlockShapesEnum name;
     public int form = 1;
 
@@ -18,31 +17,7 @@ public class Form {
         this.d = d;
         this.name = name;
 
-//        // Set color of the stones
-//        switch (name.toString().toLowerCase()) {
-//            case "j" -> {
-//                color = Color.SLATEGRAY;
-//            }
-//            case "l" -> {
-//                color = Color.DARKGOLDENROD;
-//            }
-//            case "o" -> {
-//                color = Color.INDIANRED;
-//            }
-//            case "s" -> {
-//                color = Color.FORESTGREEN;
-//            }
-//            case "t" -> {
-//                color = Color.CADETBLUE;
-//            }
-//            case "z" -> {
-//                color = Color.HOTPINK;
-//            }
-//            case "i" -> {
-//                color = Color.SANDYBROWN;
-//            }
-//        }
-        color = name.getColor();
+        Color color = name.getColor();
 
         for (Square square : getBlocks()) {
             square.setFill(color);
@@ -65,7 +40,5 @@ public class Form {
     public Square[] getBlocks() {
         return new Square[]{a, b, c, d};
     }
-
-
 
 }
